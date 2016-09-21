015efd1d11c441f110d5e54b132d0677ee87688c
YihengYao <fn1117@qq.com>
2016-09-21 22:24:37 +1200

    通过字典重构代码
    通过字典重构代码, 并且通过使用
    -(NSString *)tableView:(UITableView *)tableView
    titleForHeaderInSection:(NSInteger)section
    -(NSString *)tableView:(UITableView *)tableView
    titleForFooterInSection:(NSInteger)section
    以上两个方法, 为group添加头部与尾部的描述

--- ViewController.m
+++ ViewController.m
@@ -8,6 +8,11 @@
 
 #import "ViewController.h"
 
+//将字典的key抽出为宏
+#define kHeader @"header"   //头部标题的key
+#define kFooter @"footer"   //尾部标题的key
+#define kContent @"content" //显示内容的key
+
 @interface ViewController ()<UITableViewDataSource>
 
 @property (nonatomic, strong) UITableView *tableView;
@@ -21,11 +26,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self.view addSubview:[self loadTableView]];
-    _allList = @[@[@"🐻", @"🐈", @"🐩", @"🐱"],
-                 @[@"🌷", @"🍀", @"🍌", @"🌲"],
-                 @[@"🎧", @"📱", @"🍫"],
-                 @[@"🎻", @"🎸", @"🎹"],
-                 @[@"⚛"]
+    _allList = @[
+                 @{
+                     kHeader : @"动物",
+                     kFooter : @"这里有一些动物",
+                     kContent: @[@"🐻", @"🐈", @"🐩", @"🐱"]
+                  },
+                 @{
+                     kHeader : @"电子用品",
+                     kFooter : @"这里有一副耳机跟手机",
+                     kContent: @[@"🎧", @"📱"]
+                     },
+
+                 @{
+                     kHeader : @"乐器",
+                     kFooter : @"这里有一些乐器",
+                     kContent: @[@"🎻", @"🎸", @"🎹"]
+                     },
+                 @{
+                     kHeader : @"其他",
+                     kFooter : @"这是一个原子= =",
+                     kContent: @[@"⚛"]
+                     },
+                 @{
+                     kHeader : @"植物",
+                     kFooter : @"这里有一些植物",
+                     kContent: @[@"🌷", @"🍀", @"🍌", @"🌲"]
+                     }
                  ];
 
 
@@ -52,8 +79,16 @@
 #pragma mark 每组数据中有多少行数据
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
+    //1.取出数组中,所对应的项目的字典
+    NSDictionary *list = _allList[section];
+
+    //2.取出对应key中,content的个数
+
+    NSArray *items = list[kContent];
+
+
     //返回第section组所在的数组所包含的个数
-    return [_allList[section] count];
+    return items.count;
 }
 
 #pragma mark 每一行具体有什么数据
@@ -62,10 +97,28 @@
     //创建一个cell
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
 
-    cell.textLabel.text = _allList[indexPath.section][indexPath.row];
+    //1.取出数组中,所对应的项目的字典
+    NSDictionary *items = _allList[indexPath.section];
+
+    //2.取出对应key中的内容
+    NSArray *item = items[kContent];
+    cell.textLabel.text = item[indexPath.row];
 
     return cell;
 }
 
+#pragma mark 第section组需要显示的头部标题
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+
+    return _allList[section][kHeader];
+
+}
+
+#pragma mark 第section组需要显示的尾部标题
+-(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
+
+    return _allList[section][kFooter];
+
+}
 
 @end
